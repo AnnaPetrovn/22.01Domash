88c7d1bc6fc1f348b770f13a0522c9d8360e5ce8
Anna <annapn5@mail.ru>
2023-01-28 10:43:15 +0600

    выполнила домашнюю к прошлой субботе

---
+++ exercise1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/ 
+
+int [] YouShouldCreateArr () 
+{
+    Console.WriteLine("Введите числа через пробел");
+    string s = Console.ReadLine();
+   
+    int[] a = s.Split(' ').Select(int.Parse).ToArray();
+    for(int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine(a[i]);
+    }
+    return a;
+    }
+
+void WhatIsGreaterThanZero (int[]array)
+{
+    int greaterThanZero = 0;
+    for(int i = 0; i< array.Length; i++)
+    {
+        if(array[i]>0)
+        {
+            greaterThanZero = greaterThanZero + 1;
+        }
+    }
+    Console.WriteLine( "Количество чисел больше нуля " + greaterThanZero);
+}
+int [] arr = YouShouldCreateArr();
+WhatIsGreaterThanZero(arr);
